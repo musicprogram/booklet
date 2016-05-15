37307b2bf3be5a03d06d56a78a2dba31cf00342e
juan henao <jahenao17@misena.edu.co>
2016-05-15 07:41:21 -0500

    pagego btn

--- js/main.js
+++ js/main.js
@@ -5,7 +5,23 @@ function init(){
 			{
 				width:800,
 				height:600,
+				/* tamaño */
+
+				closed: true, /* cerrar libro */
+				covers: true, /* contraportada */
+				autoCenter: true, /* centrar */
+
+
+				pagePadding: 0,	/* padding */
+				hoverWidth:100,
+
+
 			}
 
 		);
+
+	$('#bt_go').click(function(){
+		$('#mi_libro').booklet('gotopage', $('#in_go').val());
+
+	});
 }
